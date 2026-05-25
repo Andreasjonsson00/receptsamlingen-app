05ded1af68113e055e4ab085bbf6f3f5a76dad7f
Andreas <andreasjonsson00@gmail.com>
2026-05-25 10:33:39 +0200

    refine favorites page rendering

--- client/src/pages/FavoritesPage/FavoritesPage.jsx
+++ client/src/pages/FavoritesPage/FavoritesPage.jsx
@@ -4,20 +4,24 @@ import styles from "./FavoritesPage.module.css";
 const FavoritesPage = ({ favorites, toggleFavorite }) => {
   return (
     <>
-      <h2 className={styles["favorites-page__list-title"]}>Your Favorite Recipes</h2>
-      {favorites.length === 0 ? ( // If there are no favorite recipes, show a message to the user.
-        <p>You have no favorite recipes yet. Try adding some!</p>
+      {favorites.length > 0 ? (
+        <>
+          <h2 className={styles["favorites-page__list-title"]}>
+            Your Favorite Recipes
+          </h2>
+          <ul>
+            {favorites.map((recipe) => (
+              <Recipe
+                key={recipe.id}
+                recipe={recipe}
+                isFavorite={true}
+                toggleFavorite={toggleFavorite}
+              />
+            ))}
+          </ul>
+        </>
       ) : (
-        <ul>
-          {favorites.map((recipe) => (
-            <Recipe
-              key={recipe.id}
-              recipe={recipe}
-              isFavorite={true}
-              toggleFavorite={toggleFavorite}
-            />
-          ))}
-        </ul>
+        <p>You have no favorite recipes yet. Try adding some!</p>
       )}
     </>
   );
